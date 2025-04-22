764d726aeaf47c841a20fee3d17c0c32f413aa2d
elfrouazahra <elfrouahzahra@gmail.com>
2025-04-22 23:10:29 +0100

    add Contact section and integrate it into App component

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -81,39 +81,7 @@ export const Projects = () => {
             </div>
           </div>
 
-          <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
-            <h3 className="text-xl font-bold mb-2">EWA COM²</h3>
-            <p className="text-gray-400 mb-4">
-            Ewa Community plateforme
-            </p>
-            <div className="flex flex-wrap gap-2 mb-4">
-              {[
-                "React.js",
-                "Tailwind CSS",
-                "MongoDB",
-                "Express.js",
-                "Node.js",
-              ].map((tech, key) => (
-                <span
-                  key={key}
-                  className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all"
-                >
-                  {tech}
-                </span>
-              ))}
-            </div>
-            <div className="flex justify-center items-center ">
-              <a
-                href="https://next-ewa.vercel.app/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-blue-400 hover:text-blue-300 transition-colors my-4"
-              >
-                {" "}
-                View Project {" "}
-              </a>
-            </div>
-          </div>
+          
         </div>
       </div>
       </RevealOnScroll>
